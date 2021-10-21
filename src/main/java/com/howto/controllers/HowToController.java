4fccc8d8e809415fde338fd062adc9b4223db702
meloegel <meloegel@gmail.com>
2021-10-20 22:15:40 -0400

    added getByCategory and getHowToByName into howto controller

--- src/main/java/com/howto/controllers/HowToController.java
+++ src/main/java/com/howto/controllers/HowToController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,17 @@ public class HowToController {
     public ResponseEntity<?> listAllHowTos(){
         List<HowTo> allHowTos = howToService.findAll();
         return new ResponseEntity<>(allHowTos, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/category/{category}", produces = "application/json")
+    public ResponseEntity<?> getByCategory(@PathVariable String category){
+     List<HowTo> howToList = howToService.findAllHowTosByCategory(category);
+     return new ResponseEntity<>(howToList, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/howto/{name}", produces = "application/json")
+    public ResponseEntity<?> getHowToByName(@PathVariable String name) {
+        HowTo howto = howToService.findByName(name);
+        return new ResponseEntity<>(howto, HttpStatus.OK);
     }
 }
